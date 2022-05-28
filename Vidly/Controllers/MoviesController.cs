04c11250aefd5b27621fad5676e389b164f7b2d8
beed <beed@DESKTOP-DGA0BJG>
2022-05-28 11:11:31 +0300

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        //public ViewResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek! "
+            };
+            //return View(movie);
+            //return new ViewResult();
+            return Content("Hello World!");
+        }
+    }
+}
